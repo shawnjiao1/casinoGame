b1edf7e1af8f8134fc003547a2a004738033d955
olivierAK <o.adkin-kaya@share.epsb.ca>
2019-02-04 13:52:01 -0700

    oli commit

---
+++ src/casino/GeneralUser.java
@@ -0,0 +1,35 @@
+
+package casino;
+import java.util.ArrayList;
+
+public class GeneralUser extends User {
+     
+     private String name;
+     private int income;
+     private int age;
+     private double balance;
+     public double weight;
+     public double height;
+     public boolean male;
+     public int[] gamesPlayed;
+     
+     
+     
+     
+     public GeneralUser(String name, int income, int age, double balance, double weight, double height, boolean male, String user, String pass){
+        
+         super(user,pass);
+         this.name = name;
+         this.income = income;
+         this.age = age;
+         this.balance = balance;
+         this.weight = weight;
+         this.height = height;
+         this.male = male;
+         this.gamesPlayed = new int[5];
+                
+                 
+         
+     }
+             
+}
